4fd06ef5fe9e8046bc1e3702ea63f2a5d3542fd9
marys <shlapakmv@gmail.com>
2023-09-23 16:37:22 +0300

    Edit task 3.3 in master

--- main.py
+++ main.py
@@ -5,74 +5,101 @@
 ###
 
 
-try:
-    user_number = int(input("Enter the number of the week day (1-7):"))
-    if user_number == 1:
-        print("Monday")
-    elif user_number == 2:
-        print("Tuesday")
-    elif user_number == 3:
-        print("Wednesday")
-    elif user_number == 4:
-        print("Thursday")
-    elif user_number == 5:
-         print("Friday")
-    elif user_number == 6:
-        print("Saturday")
-    elif user_number == 7:
-        print("Sunday")
-    else:
-        print("Incorrect number!")
-except ValueError as error:
-    print("Enter only integer numbers please!")
+# try:
+#     user_number = int(input("Enter the number of the week day (1-7):"))
+#     if user_number == 1:
+#         print("Monday")
+#     elif user_number == 2:
+#         print("Tuesday")
+#     elif user_number == 3:
+#         print("Wednesday")
+#     elif user_number == 4:
+#         print("Thursday")
+#     elif user_number == 5:
+#          print("Friday")
+#     elif user_number == 6:
+#         print("Saturday")
+#     elif user_number == 7:
+#         print("Sunday")
+#     else:
+#         print("Incorrect number!")
+# except ValueError as error:
+#     print("Enter only integer numbers please!")
 
 ###
 # 2. Користувач вводить два числа. Визначити, чи рівні ці числа,
 # і, якщо ні, вивести їх на екран у порядку зростання
 ###
 
-try:
-  num1 = float(input("Enter first number: "))
-  num2 = float(input("Enter second number: "))
-
-
-  if num1 == num2:
-      print(f"Numbers are equal: {num1} = {num2}")
-  elif num1 > num2:
-      print(num2, num1)
-  elif num2 > num1:
-      print(num1, num2)
-  else:
-      print("Please,enter numbers only")
-except ValueError as error:
-    print("Enter only numbers please!")
+# try:
+#   num1 = float(input("Enter first number: "))
+#   num2 = float(input("Enter second number: "))
+#
+#
+#   if num1 == num2:
+#       print(f"Numbers are equal: {num1} = {num2}")
+#   elif num1 > num2:
+#       print(num2, num1)
+#   elif num2 > num1:
+#       print(num1, num2)
+#   else:
+#       print("Please,enter numbers only")
+# except ValueError as error:
+#     print("Enter only numbers please!")
 
 ###
 # 3. Користувач вводить два числа та матем дію: + - * або /
 # Залежно від введеної матем дії вивести результат
 ###
 
+while True:
+    try:
+        num1 = float(input("Enter 1st number:"))
+        num2 = float(input("Enter 2nd number:"))
+        user_select = input("Enter the operation:")
 
-try:
-    num1 = float(input("Enter 1st number:"))
-    num2 = float(input("Enter 2nd number:"))
-    user_select = input("Enter the operation:")
-    match user_select:
-        case "+":
-            sum = num1 + num2
-            print(f"{num1} + {num2} = {sum}")
-        case "-":
-            sub = num1 - num2
-            print(f"{num1} - {num2} = {sub}")
-        case "*":
-            mul = num1 * num2
-            print(f"{num1} * {num2} = {mul}")
-        case "/":
-            div = num1 / num2
-            print(f"{num1} / {num2} = {div}")
-        case _ :
-            print("Please, enter correct operation symbol")
-except ValueError as error:
-    print("Enter only numbers please!")
-except ZeroDivisionError as error:
-    print(f"ZeroDivisionError occurred: {error}")
+        match user_select:
+            case "+":
+                sum = num1 + num2
+                print(f"{num1} + {num2} = {sum}")
+            case "-":
+                sub = num1 - num2
+                print(f"{num1} - {num2} = {sub}")
+            case "*":
+                mul = num1 * num2
+                print(f"{num1} * {num2} = {mul}")
+            case "/":
+                div = num1 / num2
+                print(f"{num1} / {num2} = {div}")
+            case _ :
+                print("Please, enter correct operation symbol")
+        while True:
+            user_select = input("Enter the operation:")
+            match user_select:
+                case "+":
+                    sum = num1 + num2
+                    print(f"{num1} + {num2} = {sum}")
+                    break
+                case "-":
+                    sub = num1 - num2
+                    print(f"{num1} - {num2} = {sub}")
+                    break
+                case "*":
+                    mul = num1 * num2
+                    print(f"{num1} * {num2} = {mul}")
+                    break
+                case "/":
+                    div = num1 / num2
+                    print(f"{num1} / {num2} = {div}")
+                    break
+                case _:
+                    print("Please, enter correct operation symbol")
+                    break
+
+
+
+
+    except ValueError as error:
+        print("Enter only numbers please!")
+    except ZeroDivisionError as error:
+        print(f"ZeroDivisionError occurred: {error}")
